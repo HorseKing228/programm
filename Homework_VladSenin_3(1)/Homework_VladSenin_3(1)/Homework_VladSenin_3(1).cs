523f8851f1a33f602f7c80db3b53bab7084134dc
HorseKing228 <vlad.seninen.05@gmail.com>
2025-06-14 17:06:18 +0300

    Add files via upload

---
+++ Homework_VladSenin_3(1)/Homework_VladSenin_3(1)/Homework_VladSenin_3(1).cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        string binaryA = Convert.ToString(a, 2).PadLeft(8, '0');
+        string binaryB = Convert.ToString(b, 2).PadLeft(8, '0');
+        Console.WriteLine($"\n{a} в двоичном коде: {binaryA}");
+        Console.WriteLine($"{b} в двоичном коде: {binaryB}");
+        string sumBinary = Binary(binaryA, binaryB);
+        Console.WriteLine("\nРезультат сложения:");
+        Console.WriteLine(sumBinary);
+        int sum = Convert.ToInt32(sumBinary, 2);
+        Console.WriteLine($"\nВ десятичном виде: {sum}");
+    }
+    static string Binary(string a, string b)
+    {
+        char[] res = new char[8];
+        int carry = 0;
+        for (int i = 7; i >= 0; i--)
+        {
+            int sum = (a[i] - '0') + (b[i] - '0') + carry;
+            res[i] = (sum % 2 == 1) ? '1' : '0';
+            carry = sum > 1 ? 1 : 0;
+        }
+        return new string(res);
+    }
+}
---
+++ Homework_VladSenin_3(1)/Homework_VladSenin_3(1)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("3.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("3.1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ab6a3a4c-8d5f-46ad-877c-0d3029e8bf8a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
