d07a1288591db144abab06381c8e1e3ba9cc5d7a
HorseKing228 <vlad.seninen.05@gmail.com>
2025-06-14 17:05:59 +0300

    Add files via upload

---
+++ Homework_VladSenin_2(3)/Homework_VladSenin_2(3)/Homework_VladSenin_2(3).cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите x [-1, 1]: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        if (x < -1 || x > 1)
+        {
+            Console.WriteLine("X должен быть в [-1, 1]");
+            return;
+        }
+        double res = 1;
+        double term = 1;
+        int f = 1;
+        while (Math.Abs(term) >= 1e-6)
+        {
+            term *= x / f;
+            res += term;
+            f++;
+        }
+        Console.WriteLine($"Примерное e^{x}: {res}");
+    }
+}
