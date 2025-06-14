1d4956203ea31fe0273ebaa20cd41e8649dbea9a
HorseKing228 <vlad.seninen.05@gmail.com>
2025-06-14 17:03:05 +0300

    Add files via upload

---
+++ Homework_VladSenin_1(1)/Homework_VladSenin_1(1)/Homework_VladSenin_1(1).cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 2; i <= n; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+    static bool IsPrime(int num)
+    {
+        if (num <= 1) 
+        { 
+            return false; 
+        }
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
