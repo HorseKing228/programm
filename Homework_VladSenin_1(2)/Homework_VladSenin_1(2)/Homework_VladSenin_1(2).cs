37e8b47e73a079ea958f99151f3f4b6613efd444
HorseKing228 <vlad.seninen.05@gmail.com>
2025-06-14 17:03:51 +0300

    Add files via upload

---
+++ Homework_VladSenin_1(2)/Homework_VladSenin_1(2)/Homework_VladSenin_1(2).cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите a:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите b:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        if (b == 0)
+        {
+            Console.WriteLine("Делить на ноль нельзя!");
+            return;
+        }
+        int res = 0;
+        int rem = Math.Abs(a);
+        int absB = Math.Abs(b);
+        while (rem >= absB)
+        {
+            rem -= absB;
+            res++;
+        }
+        if ((a < 0 && b > 0) || (a > 0 && b < 0))
+        {
+            res = -res;
+        }
+        if (a < 0)
+        {
+            rem = -rem;
+        }
+        Console.WriteLine($"Целая часть: {res}, остаток: {rem}");
+    }
+}
